c5ea455fbb2c10b515f4755bb9bd62d271cd3cf6
Maged Alosali <98350741+MagedGDEV@users.noreply.github.com>
2022-04-29 23:56:45 +0200

    add special characters and get regex from arguments while running

--- regexToNFA.py
+++ regexToNFA.py
@@ -1,7 +1,11 @@
 import jsonManager
+from tokenize import String
+import sys
 
+# jsonManager.createNewState("S2", jsonManager.NFA)
+# jsonManager.addTransition("S2", "S0", "A", jsonManager.NFA)
+# jsonManager.createJSONFile("NFA.json", jsonManager.NFA)
 
-jsonManager.createNewState("S2", jsonManager.NFA)
-jsonManager.addTransition("S2", "S0", "A", jsonManager.NFA)
-jsonManager.createJSONFile("NFA.json", jsonManager.NFA)
-
+specialChars = ["*", "+", "?", "/", "[", "]", ]
+regex = sys.argv[2]
+print (regex)
