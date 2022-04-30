9c26b0a429638c4c3eabc57d30757eddc7f537f0
Maged Alosali <98350741+MagedGDEV@users.noreply.github.com>
2022-05-01 00:42:21 +0200

    add evaluation of postfix and fix error in adding Concatenation function

--- regexToNFA.py
+++ regexToNFA.py
@@ -22,7 +22,7 @@ regex = sys.argv[2]
 regSize = len(regex)
 
 
-def addConcatinationSymbol(regex, regSize):
+def addConcatenationSymbol(regex, regSize):
     
     concRegex = ""
     for i in range (regSize - 1): 
@@ -42,8 +42,51 @@ def addConcatinationSymbol(regex, regSize):
         elif (regex[i] == ")" or regex[i] == "]") and regex[i+1] not in specialChar:
             concRegex += "."
 
+    concRegex += regex[-1]
+    return concRegex
+
 def compPrecedence (first, second):
     precedence = ["|", ".", "+", "*"]
     return precedence.index(first) > precedence.index (second)
 
-addConcatinationSymbol (regex, regSize)
+def getPostFix (concatenatedRegex, concSize):
+
+    stack = []
+    postFix = ""
+    i = 0
+    while i  < concSize:
+        concReChar = concatenatedRegex[i]
+        if concReChar not in specialChar or concReChar == "*" or concReChar == "+" or concReChar == "[":
+            if (concReChar == "["):
+                for j in range (5):
+                    postFix+=concatenatedRegex[i+j]
+                i += 4
+            else:
+                postFix += concReChar
+            
+        elif concReChar == ")":
+            while len(stack) > 0 and stack [-1] != "(":
+                postFix += stack.pop()
+            stack.pop()
+       
+        elif concReChar == "(":
+            stack.append(concReChar)
+        elif len(stack) == 0 or stack [-1] == "(" or compPrecedence(concReChar, stack[-1]):
+            stack.append(concReChar)
+        else:
+            while len(stack) > 0 and stack [-1] != "(" and not compPrecedence (concReChar, stack[-1]):
+                postFix+=stack.pop()
+            stack.append(concReChar)
+        i += 1
+
+    while len(stack) > 0:
+        postFix += stack.pop()
+
+    return postFix
+
+
+
+concatenatedRegex = addConcatenationSymbol (regex, regSize)
+postFix = getPostFix (concatenatedRegex, len(concatenatedRegex))
+print (concatenatedRegex)
+print (postFix)
