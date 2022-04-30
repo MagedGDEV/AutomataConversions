49ee337543cfc05f89c4ba408c4b494fa8ce461f
Maged Alosali <98350741+MagedGDEV@users.noreply.github.com>
2022-04-30 22:16:58 +0200

    add concatenation symbol

--- regexToNFA.py
+++ regexToNFA.py
@@ -13,72 +13,38 @@ specialChar = [
      "]",
     "{",
      "}", 
+     ".",
+     "-",
      "epsilon"
 ]
 
-orStart:int = 0
-orEnd:int = 0
-
 regex = sys.argv[2]
-regexSize: int = len(regex)
-
-# def addConcSymbol(regex): 
-#     stack = []
-#     res = ""
-#     for regChar in regex:
-#         if regChar not in specialChar or regChar == "*" or regChar == "+":
-#             res+=regChar
-#         elif regChar == ")":
-#             while len(regChar) > 0 and stack[-1] != "(":
-#                 res += stack.pop()
-#             stack.pop()
-#         elif regChar == "(":
-#             stack.append(regChar)
-#         elif len(stack) == 0 or stack[-1] == "(":
-#             stack.append(regChar)
+regSize = len(regex)
 
 
-#jsonManager.createJSONFile("NFA.json", jsonManager.NFA)
-
-@with_goto
-def computeNFA (regex, count, regSize, orStart, epsilonAdded):
-
+def addConcatinationSymbol(regex, regSize):
     
-    returnValue = [0 , -1, ""]
-    if (count  != regSize ):
-        regChar = regex[count]
-        if (regChar not in specialChar):
-
-            returnValue = computeNFA (regex, count + 1, regexSize, orStart, epsilonAdded)
-            jsonManager.addTransition("S" + str(count + epsilonAdded), "S" + str(count + 1+ epsilonAdded), regChar, jsonManager.NFA)
-            if count == regSize -1 :
-                returnValue = [0, count + epsilonAdded + 1, regChar]
-
-        elif (regChar == specialChar[1]):
-            
-            return [1, count -1 + epsilonAdded, regex[count -1]]
-
-
-    if (orStart == count and returnValue[0] == 1):
-        jsonManager.addTransition ("S" + str(count), "S" + str (returnValue[1] + 2), specialChar[-1], jsonManager.NFA)
-        jsonManager.addTransition ("S"+ str(returnValue[1] + 2), "S" + str(count + 1), regChar, jsonManager.NFA)
-        jsonManager.NFA["S" + str(count)][regChar].remove("S" + str(count + 1))
-        jsonManager.addTransition ("S" + str(returnValue[1]+1), "S" + str(returnValue[1] + 3), specialChar[-1], jsonManager.NFA)
-        jsonManager.addTransition ("S" + str(count), "S" + str(returnValue[1] + 4), specialChar[-1], jsonManager.NFA)
+    concRegex = ""
+    for i in range (regSize - 1): 
         
-        label .begin
-        oldReturn = returnValue
-        returnValue = computeNFA(regex, returnValue[1]+2, regSize, orStart,2)
-        if (returnValue[0] == 0):
-            jsonManager.addTransition("S" + str(returnValue[1]), "S" + str(oldReturn[1] + 3), specialChar[-1], jsonManager.NFA)
-        elif (returnValue[1] == 1):
-            jsonManager.addTransition ("S" + str(returnValue[1] + 1), "S" + str(oldReturn[1] + 3), specialChar[-1], jsonManager.NFA)
-            jsonManager.addTransition("S" + str(count),"S" + str(returnValue[1] + 2), specialChar[-1], jsonManager.NFA )
-            goto .begin
-    return returnValue
-
+        concRegex += regex[i]
+        if regex[i] not in specialChar:
+            if regex[i+1] not in specialChar or regex [i+1] == "(" or regex [i+1] == "[":
+                concRegex += "."
+        elif regex [i] == ")" and regex [i+1] == "(":
+            concRegex+="."
+        elif regex[i] == "]" and regex [i+1] == "[":
+            concRegex+="."
+        elif (regex [i] == "*" or regex [i] == "+") and  (regex [i+1] == "(" or regex [i+1] == "["):
+            concRegex += "."
+        elif (regex [i] == "*" or regex [i] == "+") and  regex [i+1]  not in specialChar:
+            concRegex += "." 
+        elif (regex[i] == ")" or regex[i] == "]") and regex[i+1] not in specialChar:
+            concRegex += "."
+        
     
 
+    print (concRegex)
+    
 
-computeNFA (regex, 0, regexSize, orStart, 0)
-jsonManager.createJSONFile("NFA.json", jsonManager.NFA)
+addConcatinationSymbol (regex, regSize)
